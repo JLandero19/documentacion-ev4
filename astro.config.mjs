b8dcdfa090c3ce156449911d177f7cc4e0e9f2ae
JLandero19 <118732003+JLandero19@users.noreply.github.com>
2025-04-23 13:27:18 +0200

    Se ha solucionado errores de traducción en el sidebar
    No es posible traducir las URLs con startlight, por el momento no lo permite

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,38 +13,77 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: 'Primeros pasos',
-					autogenerate: { directory: 'primerospasos' },
+					translations: {
+						en: 'First Steps',
+					},
+					collapsed: true,
+					autogenerate: {
+						directory: 'primerospasos',
+						collapsed: true,
+					},
 				},
 				{
 					label: 'Tips',
+					translations: {
+						en: 'Tips',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'tips' },
 				},
 				{
 					label: 'Tu empresa',
+					translations: {
+						en: 'Your company',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'tuempresa' },
 				},
 				{
 					label: 'Como importar mis datos',
+					translations: {
+						en: 'How to import my data',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'importardatos' },
 				},
 				{
 					label: 'Mi primer presupuesto',
+					translations: {
+						en: 'My first budget',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'primerpresupuesto' },
 				},
 				{
 					label: 'Mi primera factura',
+					translations: {
+						en: 'My first invoice',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'primerafactura' },
 				},
 				{
 					label: 'Bancos',
+					translations: {
+						en: 'Banks',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'bancos' },
 				},
 				{
 					label: 'Articulos',
+					translations: {
+						en: 'Articles',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'articulos' },
 				},
 				{
 					label: 'Contabilidad',
+					translations: {
+						en: 'Accounting',
+					},
+					collapsed: true,
 					autogenerate: { directory: 'contabilidad' },
 				},
 			],
